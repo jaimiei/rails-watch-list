db50d274a8a72a95dd9fb3285e4db37cdf3b1205
Jaimie Irvine <jaimie.irvine1990@gmail.com>
2023-01-28 16:17:36 +0000

    challenge progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get "lists", to: "lists#index"
-  get "lists/:id", to: "lists#show", as: :list
-  get "lists/new", to: "lists#new",  as: :new_list
-  post "lists", to: "lists#create", as: :create_list
-
-  get "lists/:id/bookmarks/new", to: "bookmarks#new", as: :new_bookmark
-  post "lists/:id/bookmarks", to: "bookmarks#create"
-  delete "bookmarks/:id", to: "bookmarks#destroy"
+  resources :lists do
+    resources :bookmarks, only: %i[new create destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
