a6b29aa03a5fd589324b626a81ddb77fcbd457de
Jaimie Irvine <jaimie.irvine1990@gmail.com>
2023-01-28 15:47:17 +0000

    finished lists controller

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,34 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(list_params)
+    if @bookmark.save
+      redirect_to bookmark_path(@bookmark)
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmarks_path, status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:name)
+  end
+end
